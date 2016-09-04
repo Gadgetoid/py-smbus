0f6f1920e2d3a4205ea392d251ebef91f9a56e6e
RogueM <chaosgen6@googlemail.com>
2016-09-05 00:11:55 +0100

    tweaked relative path in makedeb + tidy up

--- packaging/python3/makedeb.sh
+++ packaging/python3/makedeb.sh
@@ -5,7 +5,6 @@ setup="yes"
 cleanup="yes"
 pkgfiles=( "build" "changes" "deb" "dsc" "tar.xz" "tar.gz" )
 
-
 if [ $gettools == "yes" ]; then
     sudo apt-get update && sudo apt-get install build-essential debhelper devscripts dh-make dh-python dput gnupg
     sudo apt-get install python-all python-setuptools python3-all python3-setuptools
@@ -13,14 +12,14 @@ if [ $gettools == "yes" ]; then
 fi
 
 if [ $setup == "yes" ]; then
-    rm -R ../library/build ../library/debian &> /dev/null
-    cp -R ./debian/ ../library/
+    rm -R ../../library/build ../../library/debian &> /dev/null
+    cp -R ./debian/ ../../library/
 fi
 
-cd ../library && debuild
+cd ../../library && debuild
 
 for file in ${pkgfiles[@]}; do
-    mv ../*.$file ../packaging &> /dev/null
+    mv ../*.$file ../../packaging &> /dev/null
 done
 
 if [ $cleanup == "yes" ]; then
