f0eee6e0b64be648e5077d2e6c2802931a0e1cdd
RogueM <chaosgen6@googlemail.com>
2016-09-04 14:32:08 +0100

    added build dependencies

--- packaging/makedeb.sh
+++ packaging/makedeb.sh
@@ -9,6 +9,7 @@ pkgfiles=( "build" "changes" "deb" "dsc" "tar.xz" )
 if [ $gettools == "yes" ]; then
     sudo apt-get update && sudo apt-get install build-essential debhelper devscripts dh-make dh-python dput gnupg
     sudo apt-get install python-all python-setuptools python3-all python3-setuptools
+    sudo apt-get install i2c-tools libi2c-dev python-dev python3-dev
 fi
 
 if [ $setup == "yes" ]; then
