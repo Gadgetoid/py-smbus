4d50a1b7aab5afcd129dd1d9e02ac698d1de8a20
RogueM <chaosgen6@googlemail.com>
2016-09-05 01:37:42 +0100

    clean up unecessary stuff

--- packaging/python2/makedeb.sh
+++ packaging/python2/makedeb.sh
@@ -5,22 +5,21 @@ setup="yes"
 cleanup="yes"
 pkgfiles=( "build" "changes" "deb" "dsc" "tar.xz" "tar.gz" )
 
-
 if [ $gettools == "yes" ]; then
     sudo apt-get update && sudo apt-get install build-essential debhelper devscripts dh-make dh-python dput gnupg
-    sudo apt-get install python-all python-setuptools python3-all python3-setuptools
-    sudo apt-get install i2c-tools libi2c-dev python-dev python3-dev
+    sudo apt-get install python-dev python-setuptools
+    sudo apt-get install i2c-tools libi2c-dev
 fi
 
 if [ $setup == "yes" ]; then
-    rm -R ../library/build ../library/debian &> /dev/null
-    cp -R ./debian/ ../library/
+    rm -R ../../library/build ../../library/debian &> /dev/null
+    cp -R ./debian/ ../../library/
 fi
 
-cd ../library && debuild
+cd ../../library && debuild
 
 for file in ${pkgfiles[@]}; do
-    mv ../*.$file ../packaging &> /dev/null
+    mv ../*.$file ../packaging/python2 &> /dev/null
 done
 
 if [ $cleanup == "yes" ]; then
