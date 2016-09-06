52362b026ac0bcc67d3295d82bcc961cbbe4b1ce
RogueM <chaosgen6@googlemail.com>
2016-09-06 18:20:02 +0100

    dropped 1.1.0+26 build and new 1.1.0+27 after changing some options in control file

--- packaging/python2/makedeb.sh
+++ packaging/python2/makedeb.sh
@@ -2,13 +2,18 @@
 
 gettools="yes"
 setup="yes"
+build="yes"
 cleanup="yes"
 pkgfiles=( "build" "changes" "deb" "dsc" "tar.xz" "tar.gz" )
 
 if [ $gettools == "yes" ]; then
-    sudo apt-get update && sudo apt-get install build-essential debhelper devscripts dh-make dh-python dput gnupg
-    sudo apt-get install python-dev python-setuptools
-    sudo apt-get install i2c-tools libi2c-dev
+    sudo apt-get update
+    sudo apt-get install -y build-essential
+    sudo apt-get install -y debhelper devscripts
+    sudo apt-get install -y dh-make dh-python python-all-dev
+    sudo apt-get install -y python-dev python-setuptools
+    sudo apt-get install -y python3-dev python3-setuptools
+    sudo apt-get install -y gnupg i2c-tools libi2c-dev
 fi
 
 if [ $setup == "yes" ]; then
@@ -16,11 +21,13 @@ if [ $setup == "yes" ]; then
     cp -R ./debian/ ../../library/
 fi
 
-cd ../../library && debuild
+if [ $build == "yes" ]; then
+    cd ../../library && debuild
 
-for file in ${pkgfiles[@]}; do
-    mv ../*.$file ../packaging/python2 &> /dev/null
-done
+    for file in ${pkgfiles[@]}; do
+        mv ../*.$file ../packaging/python2 &> /dev/null
+    done
+fi
 
 if [ $cleanup == "yes" ]; then
     debuild clean
