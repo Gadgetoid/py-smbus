9d28aaad03d41ce719b5da66a9e25e887d4d837e
RogueM <chaosgen6@googlemail.com>
2016-09-06 18:34:48 +0100

    Build 1.1.0+32mu for Python 3.2 built in Raspbian Wheezy

--- packaging/python3/makedeb.sh
+++ packaging/python3/makedeb.sh
@@ -2,13 +2,18 @@
 
 gettools="yes"
 setup="yes"
+build="yes"
 cleanup="yes"
 pkgfiles=( "build" "changes" "deb" "dsc" "tar.xz" "tar.gz" )
 
 if [ $gettools == "yes" ]; then
-    sudo apt-get update && sudo apt-get install build-essential debhelper devscripts dh-make dh-python dput gnupg
-    sudo apt-get install python-all python-setuptools python3-all python3-setuptools
-    sudo apt-get install i2c-tools libi2c-dev python-dev python3-dev
+    sudo apt-get update
+    sudo apt-get install -y build-essential
+    sudo apt-get install -y debhelper devscripts
+    sudo apt-get install -y dh-make dh-python
+    sudo apt-get install -y python-all-dev python-setuptools
+    sudo apt-get install -y python3-all-dev python3-setuptools
+    sudo apt-get install -y gnupg i2c-tools libi2c-dev
 fi
 
 if [ $setup == "yes" ]; then
@@ -16,11 +21,13 @@ if [ $setup == "yes" ]; then
     cp -R ./debian/ ../../library/
 fi
 
-cd ../../library && debuild
+if [ $build == "yes" ]; then
+    cd ../../library && debuild
 
-for file in ${pkgfiles[@]}; do
-    mv ../*.$file ../packaging/python3 &> /dev/null
-done
+    for file in ${pkgfiles[@]}; do
+        mv ../*.$file ../packaging/python3 &> /dev/null
+    done
+fi
 
 if [ $cleanup == "yes" ]; then
     debuild clean
